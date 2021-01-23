b0eb570636b50c23460bbd4d1101d98282b288c8
Pankaj Jha <pankajj.jha@gmail.com>
2021-01-23 00:16:50 -0500

    updated by pankaj to test

--- src/hello.py
+++ src/hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Docker-Version2!"
+    return "Hello Docker-Version2 - update from pankaj !"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
